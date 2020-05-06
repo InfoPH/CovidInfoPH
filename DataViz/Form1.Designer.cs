2c84c8a6f41e95777532f0f84be6153adb8d3788
sitiom <rycaeli@gmail.com>
2020-05-06 17:25:28 +0800

    Initialize repo

---
+++ DataViz/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace DataViz
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      Bunifu.ToggleSwitch.ToggleState toggleState1 = new Bunifu.ToggleSwitch.ToggleState();
+      Bunifu.ToggleSwitch.ToggleState toggleState2 = new Bunifu.ToggleSwitch.ToggleState();
+      Bunifu.ToggleSwitch.ToggleState toggleState3 = new Bunifu.ToggleSwitch.ToggleState();
+      this.gunaButton1 = new Guna.UI2.WinForms.Guna2Button();
+      this.bunifuDataViz1 = new Bunifu.DataViz.WinForms.BunifuDataViz();
+      this.bunifuToggleSwitch1 = new Bunifu.ToggleSwitch.BunifuToggleSwitch();
+      this.SuspendLayout();
+      // 
+      // gunaButton1
+      // 
+      this.gunaButton1.Animated = true;
+      this.gunaButton1.AutoRoundedCorners = true;
+      this.gunaButton1.BorderRadius = 21;
+      this.gunaButton1.CheckedState.Parent = this.gunaButton1;
+      this.gunaButton1.CustomImages.Parent = this.gunaButton1;
+      this.gunaButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+      this.gunaButton1.ForeColor = System.Drawing.Color.White;
+      this.gunaButton1.HoverState.Parent = this.gunaButton1;
+      this.gunaButton1.Location = new System.Drawing.Point(238, 12);
+      this.gunaButton1.Name = "gunaButton1";
+      this.gunaButton1.ShadowDecoration.Parent = this.gunaButton1;
+      this.gunaButton1.Size = new System.Drawing.Size(180, 45);
+      this.gunaButton1.TabIndex = 17;
+      this.gunaButton1.Text = "Website";
+      // 
+      // bunifuDataViz1
+      // 
+      this.bunifuDataViz1.animationEnabled = false;
+      this.bunifuDataViz1.AxisLineColor = System.Drawing.Color.LightGray;
+      this.bunifuDataViz1.AxisXFontColor = System.Drawing.Color.Gray;
+      this.bunifuDataViz1.AxisXGridColor = System.Drawing.Color.Gray;
+      this.bunifuDataViz1.AxisXGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+      this.bunifuDataViz1.AxisYFontColor = System.Drawing.Color.Gray;
+      this.bunifuDataViz1.AxisYGridColor = System.Drawing.Color.Gray;
+      this.bunifuDataViz1.AxisYGridThickness = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+      this.bunifuDataViz1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+      this.bunifuDataViz1.Location = new System.Drawing.Point(251, 120);
+      this.bunifuDataViz1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.bunifuDataViz1.Name = "bunifuDataViz1";
+      this.bunifuDataViz1.Size = new System.Drawing.Size(536, 316);
+      this.bunifuDataViz1.TabIndex = 18;
+      this.bunifuDataViz1.Theme = Bunifu.DataViz.WinForms.BunifuDataViz._theme.theme1;
+      this.bunifuDataViz1.Title = "";
+      // 
+      // bunifuToggleSwitch1
+      // 
+      this.bunifuToggleSwitch1.Animation = 5;
+      this.bunifuToggleSwitch1.BackColor = System.Drawing.Color.Transparent;
+      this.bunifuToggleSwitch1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuToggleSwitch1.BackgroundImage")));
+      this.bunifuToggleSwitch1.Checked = true;
+      this.bunifuToggleSwitch1.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.bunifuToggleSwitch1.InnerCirclePadding = 3;
+      this.bunifuToggleSwitch1.Location = new System.Drawing.Point(516, 68);
+      this.bunifuToggleSwitch1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+      this.bunifuToggleSwitch1.Name = "bunifuToggleSwitch1";
+      this.bunifuToggleSwitch1.Size = new System.Drawing.Size(52, 28);
+      this.bunifuToggleSwitch1.TabIndex = 19;
+      toggleState1.BackColor = System.Drawing.Color.Empty;
+      toggleState1.BackColorInner = System.Drawing.Color.Empty;
+      toggleState1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(0)))), ((int)(((byte)(140)))));
+      toggleState1.BorderColorInner = System.Drawing.Color.Empty;
+      toggleState1.BorderRadius = 1;
+      toggleState1.BorderRadiusInner = 1;
+      toggleState1.BorderThickness = 1;
+      toggleState1.BorderThicknessInner = 1;
+      this.bunifuToggleSwitch1.ToggleStateDisabled = toggleState1;
+      toggleState2.BackColor = System.Drawing.Color.Empty;
+      toggleState2.BackColorInner = System.Drawing.Color.Empty;
+      toggleState2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(0)))), ((int)(((byte)(140)))));
+      toggleState2.BorderColorInner = System.Drawing.Color.Empty;
+      toggleState2.BorderRadius = 1;
+      toggleState2.BorderRadiusInner = 1;
+      toggleState2.BorderThickness = 1;
+      toggleState2.BorderThicknessInner = 1;
+      this.bunifuToggleSwitch1.ToggleStateOff = toggleState2;
+      toggleState3.BackColor = System.Drawing.Color.DodgerBlue;
+      toggleState3.BackColorInner = System.Drawing.Color.White;
+      toggleState3.BorderColor = System.Drawing.Color.DodgerBlue;
+      toggleState3.BorderColorInner = System.Drawing.Color.White;
+      toggleState3.BorderRadius = 17;
+      toggleState3.BorderRadiusInner = 11;
+      toggleState3.BorderThickness = 1;
+      toggleState3.BorderThicknessInner = 1;
+      this.bunifuToggleSwitch1.ToggleStateOn = toggleState3;
+      this.bunifuToggleSwitch1.Value = true;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(800, 450);
+      this.Controls.Add(this.bunifuToggleSwitch1);
+      this.Controls.Add(this.bunifuDataViz1);
+      this.Controls.Add(this.gunaButton1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Button gunaButton1;
+        private Bunifu.DataViz.WinForms.BunifuDataViz bunifuDataViz1;
+    private Bunifu.ToggleSwitch.BunifuToggleSwitch bunifuToggleSwitch1;
+  }
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Data Mining & Visualization [![sitiom](https://circleci.com/gh/sitiom/DataVizFormsApp/tree/master.svg?style=svg&circle-token=5793cbc27f26da4a080ec4cc69a2f3cf4bb1ec3d)](https://circleci.com/gh/sitiom/DataVizFormsApp/)
+
+A WinForms app for DataStruct PBL.
+
+## Prerequisites
+
+- This project targets .NET Framework `4.8`.
