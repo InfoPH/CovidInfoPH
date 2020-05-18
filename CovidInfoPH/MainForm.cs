b6c01bc26510a0c94db6e511b2e005cb722863a3
sitiom <rycaeli@gmail.com>
2020-05-18 19:06:28 +0800

    Fix naming convention

--- CovidInfoPH/MainForm.cs
+++ CovidInfoPH/MainForm.cs
@@ -14,17 +14,18 @@ namespace CovidInfoPH
     public partial class MainForm : Form
     {
 
-        public SheetMapper sheetMapper;
-        public Sheet sheetData;
-        public MappingResult<PatientInfo> result;
+        public SheetMapper SheetMapper;
+        public Sheet SheetData;
+        public MappingResult<PatientInfo> Result;
+
         public MainForm()
         {
             InitializeComponent();
         }
 
-        private async Task loadStuff()
+        private async Task LoadStuff()
         {
-            sheetMapper = new SheetMapper()
+            SheetMapper = new SheetMapper()
                 .AddConfigFor<PatientInfo>(cfg => cfg
                    .MapColumn(column => column.WithHeader("Age").IsRequired().MapTo(p => p.Age))
                    .MapColumn(column => column.WithHeader("DateRepConf").IsRequired().UsingFormat("dd-MM-yy").MapTo(p => p.DateConfirmed))
@@ -33,25 +34,25 @@ namespace CovidInfoPH
                        .WithDefaultValue<DateTime?>(null).MapTo(p => p.DateDied)));
 
 
-            sheetData = await new GoogleSheetAdapter().GetAsync("16g_PUxKYMC0XjeEKF6FPUBq2-pFgmTkHoj5lbVrGLhE", "'DOH Data Drop'!A1:ZZ", "AIzaSyCkssJLOPN-8WdM3HX_8N3kdq62_9hn_wA");
-            result = sheetMapper.Map<PatientInfo>(sheetData);
+            SheetData = await new GoogleSheetAdapter().GetAsync("16g_PUxKYMC0XjeEKF6FPUBq2-pFgmTkHoj5lbVrGLhE", "'DOH Data Drop'!A1:ZZ", "AIzaSyCkssJLOPN-8WdM3HX_8N3kdq62_9hn_wA");
+            Result = SheetMapper.Map<PatientInfo>(SheetData);
         }
 
-        private void loadData()
+        private void LoadData()
         {
             Bunifu.DataViz.WinForms.Canvas canvas = new Bunifu.DataViz.WinForms.Canvas();
             Bunifu.DataViz.WinForms.DataPoint data = new Bunifu.DataViz.WinForms.DataPoint(Bunifu.DataViz.WinForms.BunifuDataViz._type.Bunifu_line);
 
-            int CaseNum = 0;
+            int caseNum = 0;
 
             for (int j = 0; j < 10; j++)
             {
-                DateTime CurrentDate = result.ParsedModels[CaseNum].Value.DateConfirmed;
-                for (int i = CaseNum; CurrentDate == result.ParsedModels[i].Value.DateConfirmed; i++)
+                DateTime currentDate = Result.ParsedModels[caseNum].Value.DateConfirmed;
+                for (int i = caseNum; currentDate == Result.ParsedModels[i].Value.DateConfirmed; i++)
                 {
-                    CaseNum++;
+                    caseNum++;
                 }
-                data.addLabely(result.ParsedModels[CaseNum].Value.DateConfirmed.ToString("d"), CaseNum);
+                data.addLabely(Result.ParsedModels[caseNum].Value.DateConfirmed.ToString("d"), caseNum);
                 canvas.addData(data);
 
             }
@@ -62,12 +63,12 @@ namespace CovidInfoPH
 
         private void MainForm_Load(object sender, System.EventArgs e)
         {
-            loadStuff();
+            LoadStuff();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            loadData();
+            LoadData();
             timer1.Stop();
         }
 
