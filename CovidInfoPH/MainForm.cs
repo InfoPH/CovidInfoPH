d94043b0c8f9a05f160d4cb7afb55080e5ade6d3
sitiom <rycaeli@gmail.com>
2020-05-19 12:28:30 +0800

    Complete implementation of Historical

--- CovidInfoPH/MainForm.cs
+++ CovidInfoPH/MainForm.cs
@@ -8,6 +8,7 @@ using SheetToObjects.Lib.FluentConfiguration;
 using SheetToObjects.Adapters.GoogleSheets;
 using Bunifu.DataViz.WinForms;
 using CovidInfoPH.Models;
+using Task = System.Threading.Tasks.Task;
 
 
 namespace CovidInfoPH
@@ -15,6 +16,8 @@ namespace CovidInfoPH
     public partial class MainForm : Form
     {
         public List<Patient> Patients;
+        public Dictionary<DateTime, HistoricalInfo> Historical;
+
         public MainForm()
         {
             InitializeComponent();
@@ -22,11 +25,11 @@ namespace CovidInfoPH
 
         private async void MainForm_Load(object sender, EventArgs e)
         {
-            await LoadDataFromSheet();
+            await LoadDataFromSheetAsync();
             DisplayGraph();
         }
 
-        private async Task LoadDataFromSheet()  
+        private async Task LoadDataFromSheetAsync()
         {
             SheetMapper sheetMapper = new SheetMapper()
                 .AddConfigFor<Patient>(cfg => cfg
@@ -34,14 +37,31 @@ namespace CovidInfoPH
                    .MapColumn(column => column.WithHeader("DateRepConf").IsRequired().UsingFormat("dd-MM-yy").MapTo(p => p.DateConfirmed))
                    .MapColumn(column => column.WithHeader("DateRecover").UsingFormat("dd-MM-yy").WithDefaultValue<DateTime?>(null).MapTo(p => p.DateRecovered))
                    .MapColumn(column => column.WithHeader("DateDied").UsingFormat("dd-MM-yy")
-                       .WithDefaultValue<DateTime?>(null).MapTo(p => p.DateDied)));
+                       .WithDefaultValue<DateTime?>(null).MapTo(p => p.DateDied)))
+                .AddConfigFor<HistoricalInfo>(cfg => cfg
+                    .MapColumn(column => column.WithHeader("Cases").IsRequired().MapTo(p => p.Cases))
+                    .MapColumn(column => column.WithHeader("Deaths").IsRequired().MapTo(p => p.Deaths))
+                    .MapColumn(column => column.WithHeader("Recoveries").IsRequired().MapTo(p => p.Recoveries))
+                    .MapColumn(column => column.WithHeader("Admitted").IsRequired().MapTo(p => p.Admitted)));
 
-            
-            Sheet sheetData = await new GoogleSheetAdapter().GetAsync("16g_PUxKYMC0XjeEKF6FPUBq2-pFgmTkHoj5lbVrGLhE", "'DOH Data Drop'!A1:ZZ", "AIzaSyCkssJLOPN-8WdM3HX_8N3kdq62_9hn_wA");
-            MappingResult<Patient> result = sheetMapper.Map<Patient>(sheetData);
-            Patients = result.ParsedModels.Select(o => o.Value).OrderBy(o => o.DateConfirmed).ToList();
+            GoogleSheetAdapter adapter = new GoogleSheetAdapter();
+            //Get sheet data
+            Task<Sheet> dohDataDropSheet = adapter.GetAsync("16g_PUxKYMC0XjeEKF6FPUBq2-pFgmTkHoj5lbVrGLhE", "'DOH Data Drop'!A1:ZZ", "AIzaSyCkssJLOPN-8WdM3HX_8N3kdq62_9hn_wA");
+            Task<Sheet> historicalSheet = adapter.GetAsync("16g_PUxKYMC0XjeEKF6FPUBq2-pFgmTkHoj5lbVrGLhE",
+                "'Historical'!A1:ZZ", "AIzaSyCkssJLOPN-8WdM3HX_8N3kdq62_9hn_wA");
+            await Task.WhenAll(dohDataDropSheet, historicalSheet);
+            //Map the data accordingly
+            Patients = sheetMapper.Map<Patient>(dohDataDropSheet.Result).ParsedModels.Select(o => o.Value)
+                .OrderBy(o => o.DateConfirmed).ToList();
+            List<HistoricalInfo> historicalInfos = sheetMapper.Map<HistoricalInfo>(historicalSheet.Result).ParsedModels
+                .Select(o => o.Value).ToList();
+            List<DateTime> historicalDates = historicalSheet.Result.Rows
+                .Where(o => DateTime.TryParse((string)o.Cells[0].Value, out _) && o.Cells.Count != 1)
+                .Select(o => DateTime.Parse((string)o.Cells[0].Value)).ToList();
+            Historical = Enumerable.Range(0, historicalDates.Count)
+                .ToDictionary(i => historicalDates[i], i => historicalInfos[i]);
 
-        } 
+        }
 
         private void DisplayGraph()
         {
