b26ee2233ba7a5be01c48961a6519c19c8887fbe
sitiom <rycaeli@gmail.com>
2020-05-20 20:27:01 +0800

    Add initial splash screen

--- CovidInfoPH/Program.cs
+++ CovidInfoPH/Program.cs
@@ -14,7 +14,7 @@ namespace CovidInfoPH
             //Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            Application.Run(new Splash2());
         }
     }
 }
