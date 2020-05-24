43c4452db0d8b7a508d34c7b25dafbe37c6dc465
sitiom <rycaeli@gmail.com>
2020-05-24 18:48:26 +0800

    Add more models

---
+++ CovidInfoPH/Models/ItemSource.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidInfoPH.Models
+{
+    class ItemSource
+    {
+        public ObservableCollection<PhRegion> Regions { get; set; }
+
+        public ItemSource(Dictionary<string, RegionInfo> valuePairs)
+        {
+            Regions = GetRegions(valuePairs);
+        }
+
+        public static ObservableCollection<PhRegion> GetRegions(Dictionary<string, RegionInfo> valuePairs)
+        {
+            ObservableCollection<PhRegion> regionsList = new ObservableCollection<PhRegion>();
+            foreach (var item in valuePairs)
+            {
+                regionsList.Add(new PhRegion() { Cases = item.Value.Cases, Region = item.Key });
+            }
+            regionsList.Add(new PhRegion() { Cases = 11, Region = "Negros Island Region (NIR)" });
+            return regionsList;
+        }
+    }
+}
